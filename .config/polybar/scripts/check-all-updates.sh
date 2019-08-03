bcb79c513fbf8058fbaddf359680a78fb96189a5
Aiden Madaffri <contact@aidenmadaffri.com>
2019-08-03 08:44:46 -0500

    Fixed update script from providing ugly output when there is not internet connection

--- .config/polybar/scripts/check-all-updates.sh
+++ .config/polybar/scripts/check-all-updates.sh
@@ -7,11 +7,22 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
 fi
 
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(yay -Qum | wc -l); then
-    updates_aur=0
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    if ! updates_aur=$(yay -Qum | wc -l); then
+        updates_aur=0
+    fi
 fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    updates=$(("$updates_arch" + "$updates_aur"))
+else
+    echo "No Internet"
+    exit
+fi
+
 
 if [ "$updates" -gt 0 ]; then
     echo " $updates"
