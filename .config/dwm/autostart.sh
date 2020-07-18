db85656cfcc22c0107b203b1db748228d6c3a8ac
Aiden Madaffri <contact@aidenmadaffri.com>
2020-07-18 08:22:42 -0500

    Further laptop configurations

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-xss-lock -- slock &
 nextcloud --background &
-steam &
-thunderbird &
-discord &
-kitty --class startupterm1 &
-kitty --class startupterm2 &
-$BROWSER &
-sleep 1
-$BROWSER &
-sleep 1
-xsetroot -name "fsignal:2"
-sleep 0.5
-xsetroot -name "fsignal:1"
+if [ "$PC_TYPE" == "desktop" ]; then
+    xss-lock -- slock &
+    steam &
+    thunderbird &
+    discord &
+    kitty --class startupterm1 &
+    kitty --class startupterm2 &
+    $BROWSER &
+    sleep 1
+    $BROWSER &
+    sleep 1
+    xsetroot -name "fsignal:2"
+    sleep 0.5
+    xsetroot -name "fsignal:1"
+else
+    $BROWSER &
+fi
--- .config/dwm/autostart_blocking.sh
+++ .config/dwm/autostart_blocking.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-protonmail-bridge --noninteractive &
 feh --bg-fill --no-fehbg $HOME/Pictures/Wallpapers/wallpaper.png &
 redshift &
