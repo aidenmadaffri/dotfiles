3499202f711a340d92dd290fce845566173de10c
Aiden Madaffri <contact@aidenmadaffri.com>
2020-08-22 18:50:02 -0500

    Added corners to xautolock

--- .config/dwm/autostart_blocking.sh
+++ .config/dwm/autostart_blocking.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$PC_TYPE" == "laptop" ]; then
     pulseaudio --daemon
-    xautolock -time 5 -locker "screenoff" &
+    xautolock -time 5 -corners -000 -locker "screenoff" &
 else
-    xautolock -time 15 -locker "xset dpms force off" &
+    xautolock -time 15 -corners -000 -locker "xset dpms force off" &
 fi
 feh --bg-fill --no-fehbg $HOME/Pictures/Wallpapers/wallpaper.png &
 redshift &
